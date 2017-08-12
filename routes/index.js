db770eeab1e7c035f60c8db379e518faf5d241af
N0rR <n0rr7882@gmail.com>
2017-08-12 19:59:29 +0900

    login & register

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.render('index', { title: '/' });
+});
+
+router.get('/login', (req, res) => {
+  res.render('login', { title: '/login' });
+});
+
+router.get('/register', (req, res) => {
+  res.render('register', { title: 'register' });
+});
+
+router.get('/feeling', (req, res) => {
+  res.render('feeling', { title: '/feeling' });
+});
+
+router.get('/account', (req, res) => {
+  res.render('account', { title: '/account' });
 });
 
 module.exports = router;
